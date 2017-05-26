73383805b601b2c9c49e5dcf4602a640f5e05ac5
Antonio Arena <antonio.arena@kaust.edu.sa>
2017-05-26 15:14:26 +0300

    Remove comments so I trigger Jenkins.

--- xcrysden.rb
+++ xcrysden.rb
@@ -9,32 +9,12 @@ class Xcrysden < Formula
   version "1.5.60"
   sha256 "24e78f984d1ddae6d53f360639202e13782684a186b8a07c83d0c82471698553"
 
-  # depends_on "cmake" => :build
   bottle :unneeded
   def install
     bin.install "xcrysden"
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
-    # Remove unrecognized options if warned by configure
-    #system "./configure", "--disable-debug",
-    #                      "--disable-dependency-tracking",
-    #                      "--disable-silent-rules",
-    #                      "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    #system "make", "install" # if this fails, try separate make/make install steps
   end
 
   test do
     assert File.exist?("#{bin}/xcrysden")
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test xcrysden`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    #system "false"
   end
 end
