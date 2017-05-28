9cdeed6e9041a023450d9c0e198755ac0544b5f6
Nasr S. Hassanein <nasr.s.hassan@gmail.com>
2017-05-28 13:47:24 +0300

    remove version from xcrysden

--- xcrysden.rb
+++ xcrysden.rb
@@ -2,7 +2,6 @@ class Xcrysden < Formula
   desc "XCrySDen is a crystalline and molecular structure visualisation program aiming at display of isosurfaces and contours, which can be superimposed on crystalline structures and interactively rotated and manipulated."
   homepage "http://www.xcrysden.org/"
   url "http://www.xcrysden.org/download/xcrysden-1.5.60-linux_x86_64-semishared.tar.gz"
-  version "1.5.60"
   sha256 "24e78f984d1ddae6d53f360639202e13782684a186b8a07c83d0c82471698553"
 
   bottle :unneeded
