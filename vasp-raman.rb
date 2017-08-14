71b2598bbfc7d9ee887abfad990c0096b69390ae
Antonio <antonio.arena@kaust.edu.sa>
2017-08-14 17:20:44 +0300

    Correct URL and sha256 value.

--- vasp-raman.rb
+++ vasp-raman.rb
@@ -1,9 +1,9 @@
 class VaspRaman < Formula
   desc "Raman off-resonant activity calculator using VASP as a back-end"
   homepage "https://github.com/raman-sc/VASP"
-  url "https://github.com/raman-sc/VASP/blob/master/vasp_raman.py"
+  url "https://raw.githubusercontent.com/raman-sc/VASP/master/vasp_raman.py"
   version "0.6.0"
-  sha256 "767751e182bb925d82ce1a5b499794e5d9b63383edd312b896157ed5a8a56047"
+  sha256 "00b9e15a2a277299ae1093915bcd9cf6f4f70d26f47acd289055502bd1efcd0c"
 
   depends_on "python"
 
