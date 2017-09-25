439474e3b17ea0832523db7e9e5a3157257150bf
Antonio Arena <antonio.arena@kaust.edu.sa>
2017-09-25 16:47:34 +0300

    Fix audit issues.

--- xcrysden.rb
+++ xcrysden.rb
@@ -1,14 +1,14 @@
 class Xcrysden < Formula
-  desc "XCrySDen is a crystalline and molecular structure visualisation program aiming at display of isosurfaces and contours, which can be superimposed on crystalline structures and interactively rotated and manipulated."
+  desc "Crystalline and molecular structure visualisation program aiming at display of isosurfaces and contours."
   homepage "http://www.xcrysden.org/"
   url "http://www.xcrysden.org/download/xcrysden-1.5.60-linux_x86_64-semishared.tar.gz"
   sha256 "24e78f984d1ddae6d53f360639202e13782684a186b8a07c83d0c82471698553"
 
   bottle :unneeded
-  
+
   depends_on "tcl-tk"
   depends_on "fftw"
-  
+
   def install
     prefix.install Dir["*"]
     (bin/"xcrysden").write <<-EOS.undent
