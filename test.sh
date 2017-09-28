71f412e6421b351472b769b66a974ca09c6416fc
Antonio Arena <antonio.arena@kaust.edu.sa>
2017-09-28 17:19:53 +0300

    New test script based on how we do it on GitLab CI.

--- test.sh
+++ test.sh
@@ -6,17 +6,32 @@ then
     exit 1
 fi
 
-_formula="$(basename $1 .rb)"
+_formula="$1"
+_formula_name="$(basename $_formula .rb)"
 
-if [ ! -f  "${_formula}.rb" ]
+if [ ! -f  "${_formula}" ]
 then
-    echo "Formula <${_formula}> does not exist"
+    echo "Formula <${_formula_name}> does not exist"
     exit 1
 fi
 
 _cellar="$(brew --repository kaust-rc/apps)"
+
+if [ ! -d "${_cellar}" ]
+then
+    brew tap kaust-rc/apps
+fi
+
 _pwd="$(pwd)"
 
-ln -s "${_pwd}/${_formula}.rb" "${_cellar}/${_formula}.rb"
-brew test-bot --tap=kaust-rc/apps --junit --skip-setup "${_formula}"
-unlink "${_cellar}/${_formula}.rb"
+if [ -f "${_cellar}/${_formula}" ]
+then
+    echo "Formula already exist so I'm bailing out..."
+    exit 1
+fi
+
+ln -s "${_pwd}/${_formula}" "${_cellar}/${_formula}"
+brew install "${_formula}"
+brew audit --strict "${_formula}"
+brew test "${_formula}"
+unlink "${_cellar}/${_formula}"
