db44c6c4804728b8717564c5350dd325776259e4
asmaahassan90 <asmaa.hassan@ieeeypegypt.org>
2017-11-22 08:26:09 +0200

    Adding current version of OpenMS formula. Troubleshooting in progress...

---
+++ openms.rb
@@ -0,0 +1,36 @@
+class Openms < Formula
+  desc "Open-source software C++ library for LC-MS data management & analyses"
+  homepage "http://www.openms.de"
+  url "https://github.com/OpenMS/OpenMS/releases/download/Release2.2.0/OpenMS-2.2.0-src.tar.gz"
+  version "2.2"
+  sha256 "61e7d24890abb6f462e2f026efb0dc88b84639b228d96e919c8074d3b11d286e"
+
+  depends_on "cmake" => :build
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "autoconf" => :build
+  depends_on "boost"
+  depends_on "glpk"
+  depends_on "libsvm"
+  depends_on "bzip2"
+  depends_on "xerces-c"
+  depends_on "homebrew/science/seqan"
+  depends_on "eigen"
+  depends_on "doxygen"
+
+  def install
+    mkdir("contrib-build") unless File.directory?("contrib-build")
+    cd("contrib-build")
+    system "cmake", "-DBUILD_TYPE=WILDMAGIC", "../contrib"
+    system "cmake", "-DBUILD_TYPE=COINOR", "../contrib"
+    mkdir("OpenMS-build") unless File.directory?("OpenMS-build")
+    cd("OpenMS-build")
+    system "cmake", "-DOPENMS_CONTRIB_LIBS=../contrib-build", "-DBOOST_USE_STATIC=OFF", "--prefix=#{prefix}", "../"
+    system "make"
+  end
+
+  test do
+    system "FileInfo"
+  end
+end
