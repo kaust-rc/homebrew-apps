80c9cf98bb168a463d18fa6e60f9ad3ff522e5c5
asmaahassan90 <asmaa.hassan@ieeeypegypt.org>
2017-11-23 16:55:34 +0200

    Fixing hompage url

--- proteowizard.rb
+++ proteowizard.rb
@@ -1,6 +1,6 @@
 class Proteowizard < Formula
   desc "Modular cross-platform tools for easy proteomics data analysis"
-  homepage "https://proteowizard.sourceforge.net/index.shtml"
+  homepage "https://proteowizard.sourceforge.io/"
   url "https://raw.githubusercontent.com/kaust-rc/homebrew-apps/test-proteowizard/releases/pwiz-bin-linux-x86_64-gcc48-release-3_0_11579.tar.bz2"
   sha256 "a1236512070c4fd7818df9a1a49d5114a188030a45402b9b7a66a893b5aa16a9"
 
