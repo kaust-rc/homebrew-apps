428dfbe47f9eb363b761c515ac6e8c0b960cb3cc
asmaahassan90 <asmaa.hasssan@ieeeypegypt.org>
2017-11-25 01:04:05 +0000

    Fix build and test

--- msproteomicstools.rb
+++ msproteomicstools.rb
@@ -5,20 +5,21 @@ class Msproteomicstools < Formula
   version "0.5"
   sha256 "dbf0894c17824b61eb84882b03c7423dd614b5f4235fee76febdc8d2d426e407"
 
-  depends_on "python3" => :build
+  depends_on "python" => :build
   depends_on "openblas"
-  depends_on "numpy"
   depends_on "libxml2"
   depends_on "libxslt"
 
   def install
-    mkdir_p "#{lib}/python3.6/site-packages"
-    ENV["PYTHONPATH"]= "#{lib}/python3.6/site-packages:$PYTHONPATH"
-    system "python3", "setup.py", "install", "--prefix=#{prefix}"
+    mkdir_p "#{libexec}/lib/python2.7/site-packages"
+    ENV.prepend_create_path "PYTHONPATH", "#{libexec}/lib/python2.7/site-packages"
+    cp_r("test", prefix.to_s)
+    system "pip", "install", "cython"
+    system "pip", "install", "numpy"
+    system "python", *Language::Python.setup_install_args(libexec)
   end
 
   test do
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "python", "#{prefix}/test/test_import.py"
   end
 end
